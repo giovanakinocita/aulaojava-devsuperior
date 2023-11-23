a3e037cbbe90dea1396c0bf4624a0c8af0f87afd
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-11-23 16:35:33 -0300

    Implementando Category na memória

--- src/main/java/com/giovanakinocita/myfirstproject/MyfirstprojectApplication.java
+++ src/main/java/com/giovanakinocita/myfirstproject/MyfirstprojectApplication.java
@@ -1,13 +1,32 @@
 package com.giovanakinocita.myfirstproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.giovanakinocita.myfirstproject.entities.Category;
+import com.giovanakinocita.myfirstproject.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class MyfirstprojectApplication {
+public class MyfirstprojectApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+		
 	}
 
 }
